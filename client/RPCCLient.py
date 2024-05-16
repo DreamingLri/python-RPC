ee02d3a97e42287244bbbf7188b8c04680ebf7fe
DreamingLri <3507415455@qq.com>
2024-05-16 09:47:17 +0800

    chore: Update RPCClient.py with argparse for command line arguments and list_functions method

--- client/RPCCLient.py
+++ client/RPCCLient.py
@@ -1,6 +1,26 @@
 import socket
+from socket import *
 import json
 import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='RPC Client Starter')
+    parser.add_argument(
+        '-i', '--ip',
+        type=str,
+        help='Server IP',
+        required=True,
+        default='127.0.0.1'
+        )
+    
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        help='Server Port',
+        required=True
+        )
+    
+    return parser.parse_args()
 
 class RPCClient:
     def __init__(self, host, port):
@@ -8,15 +28,41 @@ class RPCClient:
         self.port = port
 
     def call(self, function, *args):
-        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.connect((self.host, self.port))
+        ClientSocket = socket(AF_INET, SOCK_STREAM)
+        ClientSocket.connect((self.host, self.port))
         data = {
             'function': function,
             'args': args
         }
-        socket.send(json.dumps(data).encode())
-        result = socket.recv(1024)
+        ClientSocket.send(json.dumps(data).encode())
+        result = ClientSocket.recv(1024)
         return json.loads(result.decode())
+    
+    def list_functions(self):
+        ClientSocket = socket(AF_INET, SOCK_STREAM)
+        ClientSocket.connect(('127.0.0.1', 8080))
+        data = {
+            'function': 'list'
+        }
+        ClientSocket.send(json.dumps(data).encode())
+        result = ClientSocket.recv(1024)
+        return json.loads(result.decode())
+    
+if __name__ == "__main__":
+    args = parse_args()
+    host = args.ip
+    port = args.port
+    client = RPCClient(host, port)
+
+    print("hello, here is the RPC client")
+    print("Connected to server at {}:{}".format(host, port))
+    print("Here are the available functions:")
+    print(client.list_functions())
+        
+    
+
+
+
 
 
 
