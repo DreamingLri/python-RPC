00f50c74e1cf5ec9e0deb6d756be2fcd71f93cc9
DreamingLri <3507415455@qq.com>
2024-05-16 09:47:24 +0800

    chore: Refactor registrar.py for better code organization and naming conventions

--- registrar/registrar.py
+++ registrar/registrar.py
@@ -1,20 +1,26 @@
 import socket
+from socket import *
 import json
 
 class RegisterCenter:
     def __init__(self):
-        self.function_list = {}
+        self.function_list = [
+        ]
 
     def run_register(self, ip, port):
 
         print('Starting registrar...')
-        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.bind((ip, port))
-        socket.listen()
+        RegisterSocket = socket(AF_INET, SOCK_STREAM)
+        RegisterSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        RegisterSocket.bind((ip, port))
+        RegisterSocket.listen()
         print(f'Registrar started on {ip}:{port}')
         while True:
-            connection, address = socket.accept()
+            connection, address = RegisterSocket.accept()
             self.function_service(connection)
+            print(f'Connection from {address}')
+            connection.close()
+            print(f'Connection closed')
             
 
     def list_functions(self):
@@ -24,16 +30,19 @@ class RegisterCenter:
         self.function_list.append(name)
     
     def function_service(self, connection):
-        while True:
-            data = connection.recv(1024)
-            data = json.loads(data.decode())
-            if data['function'] == 'register':
-                self.register_function(data['name'])
-                connection.send(b'Registered')
-            elif data['function'] == 'list':
-                connection.send(json.dumps(self.list_functions()).encode())
-            else:
-                connection.send(b'Function not found')
+        data = connection.recv(1024)
+        data = json.loads(data.decode())
+        if data['function'] == 'register':
+            self.register_function(data['name'])
+            connection.send(b'Registered')
+            print(self.function_list)
+        elif data['function'] == 'list':
+            connection.send(json.dumps(self.list_functions()).encode())
+        else:
+            connection.send(b'Function not found')
+                
+        
+
         
 
     def heartbeat(self):
