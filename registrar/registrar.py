dae42fceb8c2b43ff44db1cd878a0c182e64861e
DreamingLri <3507415455@qq.com>
2024-05-16 08:40:36 +0800

    chore: Refactor registrar.py for better code organization and naming conventions

--- registrar/registrar.py
+++ registrar/registrar.py
@@ -1,12 +1,45 @@
 import socket
+import json
 
-class RegistrarCenter:
+class RegisterCenter:
     def __init__(self):
         self.function_list = {}
 
-    def run_registrar(self, ip, port):
+    def run_register(self, ip, port):
+
         print('Starting registrar...')
         socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.bind((ip, port))
+        socket.listen()
+        print(f'Registrar started on {ip}:{port}')
+        while True:
+            connection, address = socket.accept()
+            self.function_service(connection)
+            
+
+    def list_functions(self):
+        return self.function_list
+    
+    def register_function(self, name):
+        self.function_list.append(name)
+    
+    def function_service(self, connection):
+        while True:
+            data = connection.recv(1024)
+            data = json.loads(data.decode())
+            if data['function'] == 'register':
+                self.register_function(data['name'])
+                connection.send(b'Registered')
+            elif data['function'] == 'list':
+                connection.send(json.dumps(self.list_functions()).encode())
+            else:
+                connection.send(b'Function not found')
+        
+
+    def heartbeat(self):
+        pass
+
 
 if __name__ == '__main__':
-    registrar = RegistrarCenter()
+    register = RegisterCenter()
+    register.run_register('127.0.0.1', 8080)
