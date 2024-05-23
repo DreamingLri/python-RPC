951c7047cdd29f8a13a50cbd3573921cc2c83f6c
DreamingLri <3507415455@qq.com>
2024-05-23 10:14:38 +0800

    chore: Refactor registrar.py for better code organization and naming conventions

--- registrar/registrar.py
+++ registrar/registrar.py
@@ -1,6 +1,7 @@
 import socket
 from socket import *
 import json
+import signal
 
 class RegisterCenter:
     def __init__(self):
@@ -8,7 +9,6 @@ class RegisterCenter:
         ]
 
     def run_register(self, ip, port):
-
         print('Starting registrar...')
         RegisterSocket = socket(AF_INET, SOCK_STREAM)
         RegisterSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -21,33 +21,43 @@ class RegisterCenter:
             print(f'Connection from {address}')
             connection.close()
             print(f'Connection closed')
-            
+                
 
     def list_functions(self):
         return self.function_list
     
     def register_function(self, name):
         self.function_list.append(name)
+
+    def delete_function(self, name):
+        self.function_list.remove(name)
     
     def function_service(self, connection):
-        data = connection.recv(1024)
-        data = json.loads(data.decode())
+        connection.settimeout(5)
+        try:
+            data = connection.recv(1024).decode()
+        except timeout:
+            print('Connection timeout')
+            connection.close()
+        
+        data = json.loads(data)
         if data['function'] == 'register':
-            self.register_function(data['name'])
-            connection.send(b'Registered')
-            print(self.function_list)
+            if data['name'] not in self.function_list:
+                self.register_function(data['name'])
+                connection.send(b'Registered')
+            else:
+                connection.send(b'Function already exists')
         elif data['function'] == 'list':
             connection.send(json.dumps(self.list_functions()).encode())
+        elif data['function'] == 'delete':
+            self.delete_function(data['name'])
+            connection.send(b'Deleted')
         else:
             connection.send(b'Function not found')
-                
-        
-
-        
+                    
 
     def heartbeat(self):
         pass
-
 
 if __name__ == '__main__':
     register = RegisterCenter()
