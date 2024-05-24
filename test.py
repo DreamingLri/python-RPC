53345180c3b2cded8d011cf281159b60f8a10465
DreamingLri <3507415455@qq.com>
2024-05-24 15:53:28 +0800

    chore: Remove unused code and comments in test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import json
+import random
+
+
+# data = (
+#     'RPC',
+#     '127.0.0.1',
+#     8080
+# )
+
+# data2 = (
+#     'RPC2',
+#     '127.0.0.1',
+#     8080
+# )
+
+# server_list = []
+# server_list.append(data)
+# server_list.append(data2)
+# server_list = json.dumps(server_list).encode()
+
+# result = json.loads(server_list)
+# print(result[0][0])
+# print(1 % 1)
+
+ip = None
+port = None
+if ip is None:
+    print("1")
